989d123d4165a297c3c52cd665fced8cc2ff9fc0
Amina0506 <amina010506@gmail.com>
2026-03-29 18:07:07 +0200

    Added main.py for deploy

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import time
+import sys
+
+def main():
+    print("Facturatie Integration Service is gestart.", flush=True)
+    print("Container draait en wacht op berichten...", flush=True)
+
+    try:
+        while True:
+            time.sleep(60)
+    except KeyboardInterrupt:
+        print("Service wordt afgesloten...")
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
